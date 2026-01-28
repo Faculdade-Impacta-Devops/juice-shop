bc45bac6290d4c50942aceb1b371ce7a368a9d0d
Fabiano Carneiro <fabianocarnerin@hotmail.com>
2026-01-27 21:22:28 -0300

    Refactor searchProducts function to use parameterized queries for improved security against SQL injection

--- data/static/codefixes/unionSqlInjectionChallenge_1.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_1.ts
@@ -2,7 +2,15 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
+    
+    const pattern = `%${criteria}%`
+    models.sequelize.query(
+      'SELECT * FROM Products WHERE ((name LIKE :pattern OR description LIKE :pattern) AND deletedAt IS NULL) ORDER BY name',
+      {
+        replacements: { pattern },
+        type: models.sequelize.QueryTypes.SELECT
+      }
+    )
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) {
